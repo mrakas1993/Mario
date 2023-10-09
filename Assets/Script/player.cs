7c3257a2c090b0039a789f0b60c9bdd0297651bf
mrakas1993 <mrakas1993@gmail.com>
2023-10-09 16:55:38 +0300

    player

--- Assets/Script/player.cs
+++ Assets/Script/player.cs
@@ -4,34 +4,17 @@ using UnityEngine;
 
 public class player : MonoBehaviour
 {
+    public Vector2 moveVector;
     public float speed = 10f;
     private Rigidbody2D rb;
-    public float horizontal;
-    private bool flipRight = true;
-    // Start is called before the first frame update
     void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
+        
     }
 
-    // Update is called once per frame
+    
     void Update()
     {
-        horizontal = Input.GetAxis("Horizontal")*speed;
-        rb.velocity = new Vector2(horizontal,rb.velocity.y);//(x,y)
-        if (horizontal > 0 && !flipRight)
-        {
-            Flip();
-        }else if(horizontal<0 && flipRight)
-        {
-            Flip();
-        }
-    }
-    void Flip()
-    {
-        flipRight = !flipRight;
-        Vector3 theScale = transform.localScale;
-        theScale.x = theScale.x * (-1);
-        transform.localScale = theScale;
+        
     }
 }
